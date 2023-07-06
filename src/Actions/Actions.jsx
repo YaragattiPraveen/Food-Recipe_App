5c79940740743cfa01166bb35072aea6936b358d
YaragattiPraveen <yaragattipraveen68@gmail.com>
2023-07-06 22:06:38 +0530

    Api setup configuration is done

--- src/Actions/Actions.jsx
+++ src/Actions/Actions.jsx
@@ -1,8 +1,45 @@
-import SendApi from "../Utils/SendApi";
 import EndPoint from "../Utils/Endpoint";
+import sendApiReq from "../Utils/SendApi";
 
 export function getHealtyRecipe() {
-  return SendApi({
+  return sendApiReq({
     url: EndPoint.byHealthyRecipe,
   });
 }
+
+export function getRecipeBySearch(){
+  return sendApiReq({
+    url: EndPoint.bySearch,
+  })
+}
+
+export function getRecipeById(){
+  return sendApiReq({
+    url: EndPoint.byID
+  })
+}
+
+export function getRecipeByCuisine(){
+  return sendApiReq({
+    url: EndPoint.byCuisine
+  })
+}
+
+export function getRecipeByDiet(){
+  return sendApiReq({
+    url: EndPoint.byDiet
+  })
+}
+
+export function getRecipeByMeal(){
+  return sendApiReq({
+    url: EndPoint.byMeal
+  })
+}
+
+export function getRecipeByDish(){
+  return sendApiReq({
+    url: EndPoint.byDish
+  })
+}
+
