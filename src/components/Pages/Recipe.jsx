c1dda0f4d82b30ae7b81b581311181053538d512
YaragattiPraveen <yaragattipraveen68@gmail.com>
2023-08-02 19:30:35 +0530

    About us page has been added

--- src/components/Pages/Recipe.jsx
+++ src/components/Pages/Recipe.jsx
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query"
 import { getRecipeByDish } from "../../Actions/Actions"
 
 const Recipe = () => {
-  const {isLoading,data} = useQuery({
-    queryKey: ["RecipeByDish"],
-    queryFn: getRecipeByDish
-  })
+  // const {isLoading,data} = useQuery({
+  //   queryKey: ["RecipeByDish"],
+  //   queryFn: getRecipeByDish
+  // })
 
-  console.log(data?.hits?.map((item) => console.log(item?.recipe.source)));
+  // console.log(data?.hits?.map((item) => console.log(item?.recipe.source)));
+ 
   return (
     <div>Recipe</div>
   )
